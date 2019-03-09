95b611a6fcb76d934c3a423ddc53610d2252ad3c
indiguy <biswas.pritam@gmail.com>
2019-03-09 10:25:53 +0530

    word wrap DP

--- src/com/pritam/daily/coding/TextJustification.java
+++ src/com/pritam/daily/coding/TextJustification.java
@@ -1,47 +1,52 @@
 /**
- * 
+ *
  */
 package com.pritam.daily.coding;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Write an algorithm to justify text. Given a sequence of words and an integer
  * line length k, return a list of strings which represents each line, fully
  * justified.
- * 
+ *
  * More specifically, you should have as many words as possible in each line.
  * There should be at least one space between each word. Pad extra spaces when
  * necessary so that each line has exactly length k. Spaces should be
  * distributed as equally as possible, with the extra spaces, if any,
  * distributed starting from the left.
- * 
+ *
  * If you can only fit one word on a line, then you should pad the right-hand
  * side with spaces.
- * 
+ *
  * Each word is guaranteed not to be longer than k.
- * 
+ *
  * For example, given the list of words ["the", "quick", "brown", "fox",
  * "jumps", "over", "the", "lazy", "dog"] and k = 16, you should return the
  * following:
- * 
+ *
  * ["the quick brown", # 1 extra space on the left "fox jumps over", # 2 extra
  * spaces distributed evenly "the lazy dog"] # 4 extra spaces distributed evenly
- * 
+ *
  * @author pribiswas
  *
  */
 public class TextJustification {
 
-	private static List<String> fullJustify(List<String> words, int maxWidth) {
-		return null;
+	private static String fullJustify(List<String> words, int maxWidth) {
+		int[][] extraSpaces = calculateExtraSpaces(words, maxWidth);
+		int[][] lineCosts = calculateLineCosts(extraSpaces);
+		int[] arrangements = findMinimumCostArrangement(lineCosts);
+		return formSolution(words, arrangements, maxWidth);
 	}
 
 	/**
 	 * Calculate extra spaces while putting words in single line. extraSpaces[i][j]
 	 * indicates no of extra spaces if words from ith to jth index are put in single
 	 * line.
-	 * 
+	 *
 	 * @param words
 	 * @param maxWidth
 	 * @return
@@ -61,9 +66,9 @@ public class TextJustification {
 
 	/**
 	 * Calculate cost of each line from given space array. lineCosts[i][j] indicates
-	 * the cost if words from ith to jth index are put in single line. We use square
-	 * to calculate the cost.
-	 * 
+	 * the cost of packing words from ith to jth index in a single line. We use
+	 * square to calculate the cost.
+	 *
 	 * @param extraSpaces
 	 * @return
 	 */
@@ -86,33 +91,63 @@ public class TextJustification {
 	}
 
 	/**
-	 * Find the minimum cost arrangement from lineCosts array.
-	 * 
+	 * Find the minimum cost arrangement from lineCosts array. minCosts[i] signifies
+	 * the optimal cost to arrange word from i to end of the list.
+	 *
 	 * @param lineCosts
 	 * @param words
 	 * @return
 	 */
 	private static int[] findMinimumCostArrangement(int[][] lineCosts) {
 		int size = lineCosts.length;
+		int[] arrangements = new int[size];
 		int[] minCosts = new int[size];
-		for (int i = 0; i < size; i++) {
-			minCosts[i] = lineCosts[0][i];
-			for (int j = 0; j < i; j++) {
-				if (lineCosts[j][i] == Integer.MAX_VALUE) {
+		for (int i = size - 1; i >= 0; i--) {
+			minCosts[i] = lineCosts[i][size - 1];
+			arrangements[i] = size;
+			for (int j = size - 1; j > i; j--) {
+				if (lineCosts[i][j - 1] == Integer.MAX_VALUE) {
 					continue;
 				}
-
+				if (minCosts[j] + lineCosts[i][j - 1] < minCosts[i]) {
+					minCosts[i] = minCosts[j] + lineCosts[i][j - 1];
+					arrangements[i] = j;
+				}
 			}
 		}
-		return null;
+		return arrangements;
+	}
+
+	/**
+	 * Form solution from given arrangements
+	 *
+	 * @param words
+	 * @param arrangements
+	 * @return
+	 */
+	private static String formSolution(List<String> words, int[] arrangements, int maxWidth) {
+		final StringBuilder slnBuilder = new StringBuilder();
+		int start = 0;
+		do {
+			int breakIndex = arrangements[start];
+			String line = words.subList(start, breakIndex).stream().collect(Collectors.joining(" "));
+			slnBuilder.append(line);
+			int trailingSpaces = maxWidth - line.length();
+			for (int i = 0; i < trailingSpaces; i++) {
+				slnBuilder.append(" ");
+			}
+			slnBuilder.append("\n");
+			start = breakIndex;
+		} while (start < words.size());
+		return slnBuilder.toString();
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		List<String> words = Arrays.asList("the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog");
+		System.out.println(fullJustify(words, 16));
 	}
 
 }
